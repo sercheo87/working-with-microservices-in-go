50f935270d4fef233c1f3136f19b54ffb65c82b1
sercheo87 <chancaysergio@gmail.com>
2023-02-04 15:52:51 -0500

    feat(microservice): Authentication service
    - Add ui authentication

--- project/users.sql
+++ project/users.sql
@@ -1,13 +1,10 @@
-
-
 --
 -- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE public.user_id_seq
     START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
+    INCREMENT BY 1 NO MINVALUE
     NO MAXVALUE
     CACHE 1;
 
@@ -22,15 +19,16 @@ SET default_table_access_method = heap;
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.users (
-    id integer DEFAULT nextval('public.user_id_seq'::regclass) NOT NULL,
-    email character varying(255),
-    first_name character varying(255),
-    last_name character varying(255),
-    password character varying(60),
+CREATE TABLE public.users
+(
+    id          integer DEFAULT nextval('public.user_id_seq'::regclass) NOT NULL,
+    email       character varying(255),
+    first_name  character varying(255),
+    last_name   character varying(255),
+    password    character varying(60),
     user_active integer DEFAULT 0,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone
+    created_at  timestamp without time zone,
+    updated_at  timestamp without time zone
 );
 
 
@@ -51,10 +49,6 @@ ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
-INSERT INTO "public"."users"("email","first_name","last_name","password","user_active","created_at","updated_at")
-VALUES
-(E'admin@example.com',E'Admin',E'User',E'$2a$12$1zGLuYDDNvATh4RA4avbKuheAMpb1svexSzrQm7up.bnpwQHs0jNe',1,E'2022-03-14 00:00:00',E'2022-03-14 00:00:00');
-
-
-
-
+INSERT INTO "public"."users"("email", "first_name", "last_name", "password", "user_active", "created_at", "updated_at")
+VALUES (E 'admin@example.com', E 'Admin', E 'User', E '$2a$12$1zGLuYDDNvATh4RA4avbKuheAMpb1svexSzrQm7up.bnpwQHs0jNe', 1,
+        E '2022-03-14 00:00:00', E '2022-03-14 00:00:00');
